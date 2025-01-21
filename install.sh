3d8980bcb93d6595c7e5200c2abf6110bfb60d86
Debojyoti-Gho <debojyotighoshmain@gmail.com>
2025-01-21 14:24:30 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# Install cmake
+# Update and install required packages (adjust for Render's base image)
+apt-get update && apt-get install -y build-essential cmake curl
+
+# Install cmake from source
 curl -LO https://cmake.org/files/v3.31/cmake-3.31.4.tar.gz
 tar -xzvf cmake-3.31.4.tar.gz
 cd cmake-3.31.4
@@ -9,4 +12,3 @@ cd ..
 
 # Install Python dependencies
 pip install --no-cache-dir -r requirements.txt
-
