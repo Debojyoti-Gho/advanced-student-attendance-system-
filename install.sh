90d551f3e11dbf92fb6b43dd2a8b18ac469bf798
Debojyoti-Gho <debojyotighoshmain@gmail.com>
2025-01-21 14:20:28 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,9 @@
 curl -LO https://cmake.org/files/v3.31/cmake-3.31.4.tar.gz
 tar -xzvf cmake-3.31.4.tar.gz
 cd cmake-3.31.4
-./bootstrap && make && sudo make install
+./bootstrap && make && make install
+cd ..
 
-# Install dlib dependency
-sudo apt-get update
-sudo apt-get install -y build-essential
-
-# Now you can install Python dependencies from requirements.txt
+# Install Python dependencies
 pip install --no-cache-dir -r requirements.txt
+
